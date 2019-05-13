23ad49b5b486498cecc2d289c396b553215f473d
zhivkoplias <homo.korvin@gmail.com>
2019-05-13 14:41:05 +0200

    groovy

--- htseq_counting/prokkagff2gtf.sh
+++ htseq_counting/prokkagff2gtf.sh
@@ -7,7 +7,7 @@ if [ "$infile" == "" ] ; then
     exit 0
 fi
 
-grep -v "#" $infile | grep "ID=" | cut -f1 -d ';' | sed 's/ID=//g' | cut -f1,4,5,7,9 |  awk -v OFS='\t' '{print $1,"PROKKA","CDS",$2,$3,".",$4,".","gene_id " $5}'
+grep -v "#" $infile | grep "ID=" | cut -f1 -d ';' | sed 's/ID=//g' | cut -f1,4,5,7,12 |  awk -v OFS='\t' '{print $1,"PROKKA","CDS",$2,$3,".",$4,".","gene_id " $5,"gene_name " $6}'
 
 # Use indexed & sorted bam file from anvi-init-bam (or default outut from samtools sort)
 # htseq-count -r pos -t CDS -f bam Sample01_indexed.bam test.gtf
